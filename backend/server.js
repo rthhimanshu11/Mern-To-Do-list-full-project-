5406c06775296ccce5c394beb760e24f517ab016
rthhimanshu11 <rthhimanshu11@gmail.com>
2026-03-26 18:23:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
