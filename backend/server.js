e0ff8a4ce03ae6f2112c522dba77e7027fd297af
rthhimanshu11 <rthhimanshu11@gmail.com>
2026-03-26 18:47:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
