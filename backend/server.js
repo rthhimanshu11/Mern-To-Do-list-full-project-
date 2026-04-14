e844aefb98983aaa6c16621bd9965a84e80d46fa
himanshurathore0110 <himanshurathore0110@gmail.com>
2026-04-15 03:21:24 +0530

    Update MongoDB Atlas connection and dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoDB")
+mongoose.connect("process.env.MONGO_URI")
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
