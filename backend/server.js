a0b2e6e56886a64bdf22a9e73c01c4a420c15a85
himanshurathore0110 <himanshurathore0110@gmail.com>
2026-04-15 03:34:51 +0530

    Update MongoBD_URI

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("process.env.MONGO_URI")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
