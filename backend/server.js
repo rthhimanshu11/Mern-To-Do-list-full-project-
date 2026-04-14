c0b39c2b22f71ecea33080d3120035a636000400
himanshurathore0110 <himanshurathore0110@gmail.com>
2026-04-15 03:29:51 +0530

    Update MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+
 
 const app = express();
 app.use(express.json());
